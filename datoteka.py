c61c169cf1ba0eb27edd8d9fc92e447bd44d09a5
Viktor-Alojzije Ćorić <viktorcoric@protonmail.ch>
2021-01-19 21:20:11 +0100

    Dodana provjera duplikata

--- datoteka.py
+++ datoteka.py
@@ -1,15 +1,23 @@
 import os.path
 import sys
 import csv
+import subprocess
 
 if os.path.exists("podaci.csv") == 0:
     f = open("podaci.csv", "x")
     f.close()
 
+def provjeraDuplikata():
+    f = open("podaci.csv", "r+")
+    citaj = csv.reader(f, delimiter=',')
+    for redak in citaj:
+        if redak[0] == sys.argv[2]:
+            komanda = "sed -i -e 's/" + redak[1] + "/" + sys.argv[3] + "/' podaci.csv"
+            subprocess.call([komanda], shell=True)
+            return 1
+
 def zapis():
-
     f = open("podaci.csv", "a")
-
     string = sys.argv[2] + "," +  sys.argv[3] + "\n"
     f.write(string)
     f.close()
@@ -18,9 +26,12 @@ def pretraga():
     f = open("podaci.csv", "r")
     citaj = csv.reader(f, delimiter=',')
     for redak in citaj:
-        return redak[1]
+        if redak[0] == sys.argv[2]:
+            return redak[1]
 
 if sys.argv[1] == 'PUBLISH':
+    if provjeraDuplikata():
+        sys.exit(1)
     sys.exit(zapis())
 
 if sys.argv[1] == 'GET':
--- klijentTest.py
+++ klijentTest.py
@@ -5,7 +5,7 @@ serverPort = 14010
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((serverName, serverPort))
-zahtjev = "PUBLISH\r\nEMAIL: josip.ivic@gmail.com\r\nECDH KEY PART: lndoigfweoifhow\r\n"
+zahtjev = "PUBLISH\r\nEMAIL: ivo.ivic@gmail.com\r\nECDH KEY PART: 2ifdasfoaoifa\r\n"
 #zahtjev = "GET\r\nEMAIL: ivo.ivic@gmail.com\r\n"
 sock.send(zahtjev.encode())
 rezultat = sock.recv(1024)
